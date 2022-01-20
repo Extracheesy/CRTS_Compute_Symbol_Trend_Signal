4cf2d956fd520603b7555e03204dff32a91657ba
Extracheesy <78427936+Extracheesy@users.noreply.github.com>
2022-01-20 11:32:06 +0100

    ___

--- manage_data/tools.py
+++ manage_data/tools.py
@@ -17,10 +17,37 @@ def mk_directories():
     if not os.path.exists(config.OUTPUT_DIR):
         os.makedirs(config.OUTPUT_DIR)
 
+    if (config.MULTITHREADING == True):
+        if not os.path.exists(config.MULTITHREADING_POOL):
+            os.makedirs(config.MULTITHREADING_POOL)
+        else:
+            for f in os.listdir(config.MULTITHREADING_POOL):
+                os.remove(os.path.join(config.MULTITHREADING_POOL, f))
+
     if (config.COLAB == True):
         if not os.path.exists(config.COLAB_OUTPUT_CRTS):
             os.makedirs(config.COLAB_OUTPUT_CRTS)
         if not os.path.exists(config.COLAB_OUTPUT_DIR):
             os.makedirs(config.COLAB_OUTPUT_DIR)
+
+def split_df(df, size_split):
+    return df[:size_split], df[size_split:]
+
+def split_list_into_list(df):
+    # split a df into a list of breakdown df
+    len_df = len(df)
+    len_split_df = int(len_df / config.MULTITHREADING_NB_SPLIT_DF)
+
+    rest_of_the_df = df.copy()
+    global_split_list = []
+
+    for i in range(config.MULTITHREADING_NB_SPLIT_DF):
+        splited_df, rest_of_the_df = split_df(rest_of_the_df, len_split_df)
+        global_split_list.append(splited_df)
+
+    if len(rest_of_the_df) > 1:
+        global_split_list.append(rest_of_the_df)
+
+    return global_split_list
 
 
--- scraping/scrap_yahoo.py
+++ scraping/scrap_yahoo.py
@@ -1,13 +1,17 @@
 from yahoo_fin import stock_info as si
 import yfinance as yf
 import datetime
+import concurrent.futures
+import uuid
+
+import config
+
+from tools import split_list_into_list
+from merge import merge_csv_to_df
 
 def use_yfinance_api(df):
     list_stocks = df.symbol.tolist()
     df = df.set_index('symbol')
-
-
-
 
     for stock in list_stocks:
         try:
@@ -27,6 +31,11 @@ def use_yfinance_api(df):
 
     return df
 
+def use_yfinance_multi_api(df):
+    df = use_yfinance_api(df)
+    filename = config.MULTITHREADING_POOL + str(uuid.uuid4()) + '_result.csv'
+    df.to_csv(filename)
+
 def use_yahoo_fin_api(df):
     return df
 
@@ -38,10 +47,18 @@ def get_yahoo_recommendation(df):
     df["T_r_Mean"] = ""
     START_TIME = datetime.datetime.now().now()
     print("get YahooF recom")
+    if config.MULTITHREADING == True:
+        global_split_list = split_list_into_list(df)
 
-    df = use_yfinance_api(df)
+        with concurrent.futures.ThreadPoolExecutor(max_workers=config.MULTITHREADING_NUM_THREADS) as executor:
+            executor.map(use_yfinance_multi_api, global_split_list)
 
-    print("End runtime: ", datetime.datetime.now().now() - START_TIME)
+        df = merge_csv_to_df(config.MULTITHREADING_POOL, "*_result.csv")
+
+    else:
+        df = use_yfinance_api(df)
+
+    print("runtime: ", datetime.datetime.now().now() - START_TIME)
 
     # df = use_yahoo_fin_api(df)
 
